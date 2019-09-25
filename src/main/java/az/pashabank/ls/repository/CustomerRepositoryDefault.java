d9efb5b6a6073a52022d8662c1c2acf3111e2c60
IsaVerdiyev <verdiyev.isa1992@gmail.com>
2019-09-25 11:14:11 +0400

    Now when deleting or updating item customer is thrown if customer does'nt exist

--- src/main/java/az/pashabank/ls/repository/CustomerRepositoryDefault.java
+++ src/main/java/az/pashabank/ls/repository/CustomerRepositoryDefault.java
@@ -14,51 +14,51 @@ import java.util.stream.Collectors;
 
 @Repository
 @Profile("default")
-public class CustomerRepositoryDefault implements CustomerRepository{
+public class CustomerRepositoryDefault implements CustomerRepository {
 
     private static final Logger logger = LoggerFactory.getLogger(CustomerRepositoryDev.class);
 
     Long id;
 
-    HashMap<Long, CustomerDto> customers = new HashMap<>(){
+    HashMap<Long, CustomerDto> customers = new HashMap<>() {
         {
             put(1L, CustomerDto.builder().id(1L).firstName("Azer").lastName("Sharifov").build());
             put(2L, CustomerDto.builder().id(2L).firstName("Orxan").lastName("Mutalibov").build());
         }
     };
 
-    public CustomerRepositoryDefault(){
+    public CustomerRepositoryDefault() {
         id = (long) customers.size();
     }
 
-    public CustomerDto findById(Long id){
+    public CustomerDto findById(Long id) {
         logger.info("CustomerRepository.findById(Long id) was called with id = " + id);
-        return customers.get(id);
+        CustomerDto foundCustomer;
+        foundCustomer = customers.get(id);
+        if (foundCustomer == null) {
+            logger.warn("Customer was not found with id = " + id);
+            throw new EntityNotFoundException("Customer not found with id = " + id.toString());
+        }
+        return foundCustomer;
     }
 
-    public List<CustomerDto> findAll(){
+    public List<CustomerDto> findAll() {
         logger.info("CustomerRepository.findAll() was called");
         return customers.values().stream().collect(Collectors.toList());
     }
 
-    public void deleteById(Long id){
-        CustomerDto customerDto = findById(id);
+    public void deleteById(Long id) {
         logger.info("CustomerRepository.deleteById(Long id) was called with id = " + id);
-        if(customerDto == null){
-            logger.warn("Coudn't delete customerDto with parameter id. Was not found");
-            throw  new EntityNotFoundException();
-        }
-
+        findById(id);
         customers.remove(id);
     }
 
-    public CustomerDto save(CustomerDto customerDto){
+    public CustomerDto save(CustomerDto customerDto) {
         logger.info("CustomerRepository.save(CustomerDto c) was called with c.id = " + customerDto.getId());
-        if(customerDto.getId() != null && (customerDto.getId() >= id || !customers.containsKey(customerDto.getId()))){
-            logger.warn("Customer with id = "+ customerDto.getId() + " was not found. Can't be updated");
+        if (customerDto.getId() != null && (customerDto.getId() >= id || !customers.containsKey(customerDto.getId()))) {
+            logger.warn("Customer with id = " + customerDto.getId() + " was not found. Can't be updated");
             throw new NoSuchElementException();
-        }
-        else if (customerDto.getId() == null){
+        } else if (customerDto.getId() == null) {
             customerDto.setId(++id);
         }
         customers.put(customerDto.getId(), customerDto);
